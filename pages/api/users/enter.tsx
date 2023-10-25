4460e0d27f14b7e1c03109c57b6dc5faeb5fbe07
UHquokka <bananamayong@gmail.com>
2023-10-25 23:58:16 +0900

    8.1 Form Submission - 1

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,14 +2,9 @@ import client from "@/libs/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function handler(req: NextRequest, res: NextResponse) {
-    await client.user.create({
-        data: {
-            name: "test",
-            email: "email"
-        },
-    });
-    res.json({
-        ok: true,
-    });
-}
 
+    console.log(req.body)
+    res.status(200).end();
+
+
+}
